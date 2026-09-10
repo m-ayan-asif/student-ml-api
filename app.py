e7f0095889eec80f2f9ef329352f24356467b16a
m-ayan-asif <m.ayanasif2003@gmail.com>
2026-09-10 12:20:18 +0500

    feat: add health and predict endpoints

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+with open("VERSION") as f:
+    VERSION = f.read().strip()
+
+app = FastAPI()
+
+
+class PredictRequest(BaseModel):
+    value: float
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/health")
+async def health():
+    return {
+        "status": "healthy",
+        "application": "student-ml-api",
+        "version": VERSION
+    }
+
+
+@app.post("/predict")
+async def predict(req: PredictRequest):
+    prediction = req.value * 2
+    return {"input": req.value, "prediction": prediction}
